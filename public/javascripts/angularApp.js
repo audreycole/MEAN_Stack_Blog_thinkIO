97f5794fab0b7606bbc8da734b4475f17615ebce
Audrey Cole <audreycole88@yahoo.com>
2016-04-05 10:36:07 -0500

    finished basic mean stack blog

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -30,13 +30,87 @@ function($stateProvider, $urlRouterProvider) {
           return posts.get($stateParams.id);
         }]
       }
+    })
+
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
+    })
+    
+    .state('register', {
+      url: '/register',
+      templateUrl: '/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
     });
 
   $urlRouterProvider.otherwise('home');
 }]);
 
+/* Create a Factory for authenticating users */
+app.factory('auth', ['$http', '$window', function($http, $window){
+  var auth = {};
+
+  auth.saveToken = function (token){
+    $window.localStorage['blog-token'] = token;
+  };
+
+  auth.getToken = function (){
+    return $window.localStorage['blog-token'];
+  }
+
+  auth.isLoggedIn = function(){
+    var token = auth.getToken();
+
+    if(token){
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function(){
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user){
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user){
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function(){
+    $window.localStorage.removeItem('blog-token');
+  };
+
+  return auth;
+}]);
+
 /* Create a Factory for posts so we can access it anywhere, and only have on copy */
-app.factory('posts', ['$http', function($http) {
+app.factory('posts', ['$http', 'auth', function($http, auth) {
   var o = {
     posts: []
   }
@@ -57,28 +131,31 @@ app.factory('posts', ['$http', function($http) {
 
   /* Create a post that will be added to our database (POST '/posts') */
   o.create = function(post) {
-    return $http.post('/posts', post).success(function(data) { // POST to database
-      o.posts.push(data); // add this to current lists of posts
-    }); 
+    return $http.post('/posts', post, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      o.posts.push(data);
+    });
   };
 
-  /* Upvote a post (make call to database) */
   o.upvote = function(post) {
-    return $http.put('/posts/' + post._id + '/upvote').success(function(data) {
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
       post.upvotes += 1;
     });
   };
 
-  /* Add a comment (make POST to database) */
   o.addComment = function(id, comment) {
-    return $http.post('/posts/' + id + '/comments', comment);
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    });
   };
 
-  /* Upvote a comment (make call to database) */
   o.upvoteComment = function(post, comment) {
-    //console.log("id: " + comment._id);
-    //console.log("url: " + '/posts/' + post._id + '/comments/' + comment._id + '/upvote');
-    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function(data){
+    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
       comment.upvotes += 1;
     });
   };
@@ -86,12 +163,49 @@ app.factory('posts', ['$http', function($http) {
   return o;
 }]);
 
+/* Create a Controller that deals with authentication */
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}]);
+
+/* Create a Controller for a nav-bar to have at the top */
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
+
 
 /* Controllers should be used purely to wire up services, dependencies and other objects, 
  and assign them to the view via scope. */
-app.controller('MainCtrl', ['$scope', 'posts',
+app.controller('MainCtrl', ['$scope', 'posts', 'auth',
 
-function($scope, posts){
+function($scope, posts, auth){
+
+  $scope.isLoggedIn = auth.isLoggedIn;
 
   $scope.posts = posts.posts; // get posts key in hash of posts factory
 
@@ -123,9 +237,11 @@ function($scope, posts){
 
 /*Posts Controller */
 // Add in post so we can access post database methods
-app.controller('PostsCtrl', ['$scope', '$stateParams','posts', 'post',
+app.controller('PostsCtrl', ['$scope', '$stateParams','posts', 'post', 'auth',
 
-function($scope, $stateParams, posts, post) {
+function($scope, $stateParams, posts, post, auth) {
+  
+  $scope.isLoggedIn = auth.isLoggedIn;
   
   //$scope.post = posts.posts[$stateParams.id];
   $scope.post = post;
