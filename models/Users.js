59d2b47c7d3ba419ab980af8043a3d1dbb225f9a
Audrey Cole <audreycole88@yahoo.com>
2016-04-05 09:20:11 -0500

    finished user model

---
+++ models/Users.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  hash: String,
+  salt: String
+});
+
+// Set the password's salt and hash
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+// Check if it matches the password
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+};
+
+// Generate a json web token that will keep them logged in
+UserSchema.methods.generateJWT = function() {
+
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
+};
+
+mongoose.model('User', UserSchema);
